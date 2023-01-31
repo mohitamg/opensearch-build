a2493821a6db16969000fd8032ff5ec015a8f64e
Mohit Kumar <mohitamg@amazon.com>
2023-01-31 09:51:34 +0530

    Removing termination
    Signed-off-by: Mohit Kumar <mohitamg@amazon.com>

--- src/test_workflow/perf_test/perf_test_cluster.py
+++ src/test_workflow/perf_test/perf_test_cluster.py
@@ -135,4 +135,5 @@ class PerfTestCluster(TestCluster):
             cluster.start()
             yield cluster
         finally:
-            cluster.terminate()
+            #cluster.terminate()
+            logging.info("Terminated")
